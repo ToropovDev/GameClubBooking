e5ad592541de8d683c101bcd07323207e3943bcc
Arseniy <toropovdev@yandex.ru>
2024-05-26 22:00:07 +0500

    Add auth

--- backend/src/auth/models.py
+++ backend/src/auth/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
-
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
 from sqlalchemy import MetaData, Integer, String, TIMESTAMP, ForeignKey, Table, Column, JSON, Boolean
+from backend.src.database import base
 
 metadata = MetaData()
 
@@ -11,6 +12,19 @@ role = Table(
     Column("name", String, nullable=False),
     Column("permissions", JSON),
 )
+
+
+class User(SQLAlchemyBaseUserTable[int], base):
+    id = Column(Integer, primary_key=True)
+    email = Column(String, nullable=False)
+    username = Column(String, nullable=False)
+    registered_at = Column(TIMESTAMP, default=datetime.utcnow)
+    role_id = Column(Integer, ForeignKey(role.c.id))
+    hashed_password: str = Column(String(length=1024), nullable=False)
+    is_active: bool = Column(Boolean, default=True, nullable=False)
+    is_superuser: bool = Column(Boolean, default=False, nullable=False)
+    is_verified: bool = Column(Boolean, default=False, nullable=False)
+
 
 user = Table(
     "user",
